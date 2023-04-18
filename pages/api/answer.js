7c371b22b955d99773c43649a8ac422defdaf784
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-04-17 20:54:06 -0400

    fix vercel deployment issues

--- pages/api/answer.js
+++ pages/api/answer.js
@@ -3,32 +3,60 @@
 import { OpenAI } from "langchain/llms";
 import { loadQAChain } from "langchain/chains";
 import { Document } from "langchain/document";
-import puppeteer from 'puppeteer'
-
 
 export default async function handler(req, res) {
-  const browser = await puppeteer.launch({ headless: true});
-  const page = await browser.newPage();
 
+  const body = JSON.parse(req.body)
+  const websiteResponse = await fetch(body.website)
+
+  if(!websiteResponse.ok) {
+
+    res.status(404).json('Website not found')
+
+  }
+
+  let chrome = {};
+  let puppeteer;
+
+  if(process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+
+    chrome = require("chrome-aws-lambda");
+    puppeteer = require("puppeteer-core");
+
+  }else {
+
+    puppeteer = require("puppeteer")
+
+  }
+
+  let options = {};
+
+  if(process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    }
+
+  }
+
+  const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
 
   try {
     const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.9, concurrency: 10, cache: true });
-    const body = JSON.parse(req.body)
-    const websiteResponse = await fetch(body.website)
+    const chain = loadQAChain(model);
 
-    if (websiteResponse.ok) {
-      const chain = loadQAChain(model);
-
-      await page.goto(body.website, { timeout: 180000 });
-      const text = await page.$eval(('*'), (el) => el.innerText)
-      const filteredText = text.match(/(.+?\.)|(.+?\?)/g);
+    await page.goto(body.website, { timeout: 180000 });
+    const text = await page.$eval(('*'), (el) => el.innerText)
+    const filteredText = text.match(/(.+?\.)|(.+?\?)/g);
       
-      const docs = filteredText.map(sentence => new Document({ pageContent: sentence }))
+    const docs = filteredText.map(sentence => new Document({ pageContent: sentence }))
 
-      res.json(await chain.call({ question: body.question, input_documents: docs }))
-    } else {
-      res.status(404).json('Website not found')
-    }
+    res.json(await chain.call({ question: body.question, input_documents: docs }))
 
   } catch (error) {
     res.json(error)
