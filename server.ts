8b64c79e0589b920fb613d8a6e8be240d1427a0e
andrea-everett <andrea.dawn.everett@gmail.com>
2023-04-28 19:12:37 -0400

    change PORT

--- server.ts
+++ server.ts
@@ -21,82 +21,82 @@ dotenv.config();
 const upload = multer();
 
 server
-	.prepare()
-	.then(() => {
-		const app = express();
+  .prepare()
+  .then(() => {
+    const app = express();
 
-		app.use(express.json());
-		app.use(upload.array('input'));
+    app.use(express.json());
+    app.use(upload.array('input'));
 
-		app.get('*', (req, res) => handle(req, res));
+    app.get('*', (req, res) => handle(req, res));
 
-		app.listen(port, () => {
-			console.log(`> Ready on ${port}`);
-		});
+    app.listen(process.env.PORT || port, () => {
+      console.log(`> Ready on ${port}`);
+    });
 
-		app.post('/api/answer', async (req, res) => {
-			const { website, question } = req.body;
+    app.post('/api/answer', async (req, res) => {
+      const { website, question } = req.body;
 
-			// validate the requested URL
-			const websiteResponse = await fetch(website);
-			if (!websiteResponse.ok) return res.status(404).json('Website not found');
+      // validate the requested URL
+      const websiteResponse = await fetch(website);
+      if (!websiteResponse.ok) return res.status(404).json('Website not found');
 
-			// instantiate puppeteer
-			const browser = await puppeteer.launch({
-				headless: 'new',
-				ignoreHTTPSErrors: true,
-				args: [
-					'--disable-gpu',
-					'--disable-dev-shm-usage',
-					'--disable-setuid-sandbox',
-					'--no-first-run',
-					'--no-sandbox',
-					'--no-zygote',
-					'--single-process',
-				],
-			});
-			const page = await browser.newPage();
+      // instantiate puppeteer
+      const browser = await puppeteer.launch({
+        headless: 'new',
+        ignoreHTTPSErrors: true,
+        args: [
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--disable-setuid-sandbox',
+          '--no-first-run',
+          '--no-sandbox',
+          '--no-zygote',
+          '--single-process',
+        ],
+      });
+      const page = await browser.newPage();
 
-			// process the page
-			try {
-				// instantiate the model
-				const model = new OpenAI({
-					openAIApiKey: process.env.OPENAI_API_KEY,
-					temperature: 0.9,
-					concurrency: 10,
-					cache: true,
-				});
-				const chain = loadQAChain(model);
+      // process the page
+      try {
+        // instantiate the model
+        const model = new OpenAI({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+          temperature: 0.9,
+          concurrency: 10,
+          cache: true,
+        });
+        const chain = loadQAChain(model);
 
-				// navigate to the page via Puppeteer
-				await page.goto(website, { timeout: 180000 });
+        // navigate to the page via Puppeteer
+        await page.goto(website, { timeout: 180000 });
 
-				// scrape the page 🕷️
-				// @ts-ignore
-				const text = await page.$eval('*', (el) => el.innerText);
-				const filteredText = text.match(/(.+?\.)|(.+?\?)/g);
-				const docs = filteredText.map(
-					(pageContent) => new Document({ pageContent })
-				);
+        // scrape the page 🕷️
+        // @ts-ignore
+        const text = await page.$eval('*', (el) => el.innerText);
+        const filteredText = text.match(/(.+?\.)|(.+?\?)/g);
+        const docs = filteredText.map(
+          (pageContent) => new Document({ pageContent })
+        );
 
-				// close Puppeteer
-				await browser.close();
+        // close Puppeteer
+        await browser.close();
 
-				// evaluate the data
-				const data = await chain.call({
-					question,
-					input_documents: docs,
-				});
+        // evaluate the data
+        const data = await chain.call({
+          question,
+          input_documents: docs,
+        });
 
-				res.send(data);
-			} catch (error) {
-				console.error('Error occurred handling', req.url);
-				console.log(error);
-				res.end('internal server error');
-			}
-		});
-	})
-	.catch((exception) => {
-		console.log(exception.stack);
-		process.exit(1);
-	});
+        res.send(data);
+      } catch (error) {
+        console.error('Error occurred handling', req.url);
+        console.log(error);
+        res.end('internal server error');
+      }
+    });
+  })
+  .catch((exception) => {
+    console.log(exception.stack);
+    process.exit(1);
+  });
