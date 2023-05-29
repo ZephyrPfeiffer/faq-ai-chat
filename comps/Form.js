c48e2936defc54b105594a140b8a74b5612e50b6
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-05-29 12:48:51 -0400

    remove labels and add placeholder text to input elements

--- comps/Form.js
+++ comps/Form.js
@@ -5,11 +5,9 @@ const Form = ({ handleSubmit, onSubmit, register, errors }) => {
   return (
     <section className={styles.form_container}>
       <form className={styles.question_form} onSubmit={handleSubmit((data) => onSubmit(data))}>
-        <label htmlFor="website-input">Website:</label>
-        <input id="website-input" className={styles.question_input} {...register("website")} type="text" />
+        <input id="website-input" className={styles.question_input} {...register("website")} type="text" placeholder="Website URL" />
         <span>{errors.website?.message}</span><br />
-        <label htmlFor="question-input">Question:</label>
-        <input id="question-input" className={styles.question_input} {...register("question")} type="text" />
+        <input id="question-input" className={styles.question_input} {...register("question")} type="text" placeholder="Question" />
         <span>{errors.question?.message}</span><br />
         <input type="button" value="S u b m i t" className={styles.form_button} onClick={handleSubmit((data) => onSubmit(data))} />
       </form>
