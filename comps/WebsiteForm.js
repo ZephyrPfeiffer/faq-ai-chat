8b1a47f45e42ddaf0b2956c8ce3bc83b6494af96
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-06-22 09:59:02 -0400

    adjust layout of website form

--- comps/WebsiteForm.js
+++ comps/WebsiteForm.js
@@ -4,9 +4,9 @@ import styles from './WebsiteForm.module.css'
 const Form = ({ handleSubmit, onSubmit, register, errors }) => {
   return (
     <section className={styles.form_container}>
-      <form className={styles.question_form} onSubmit={handleSubmit((data) => onSubmit(data))}>
-        <input id="website-input" className={styles.question_input} {...register("website")} type="text" placeholder="Website URL" />
-        <span>{errors.website?.message}</span><br />
+      <form className={styles.website_form} onSubmit={handleSubmit((data) => onSubmit(data))}>
+        <input id="website-input" className={styles.website_input} {...register("website")} type="text" placeholder="Website URL" />
+        <span className={styles.website_error}>{errors.website?.message}</span><br />
       </form>
     </section>
   )
