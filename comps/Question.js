3cd99f30cdac728aa5b5f008ff9e7118141348c5
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-06-23 09:07:42 -0400

    remove hard coded text from question component

--- comps/Question.js
+++ comps/Question.js
@@ -3,7 +3,7 @@ import styles from './Question.module.css'
 const Question = ({question}) => {
   return (
     <div className='question-container'>
-        <p className={styles.line}>Question: {question}</p>
+        <p className={styles.line}>{question}</p>
     </div>
     
   )
