d4873b03bb9bce671a2bed8c4e36083b5ccfebf4
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-06-23 08:36:49 -0400

    add functionality to conditionally render website input error message

--- comps/WebsiteForm.js
+++ comps/WebsiteForm.js
@@ -2,11 +2,14 @@ import React from 'react';
 import styles from './WebsiteForm.module.css'
 
 const Form = ({ handleSubmit, onSubmit, register, errors }) => {
+
+  let errorDisplay = (errors.website?.message ? <span className={styles.website_error}>{errors.website?.message}</span> : null)
+
   return (
     <section className={styles.form_container}>
       <form className={styles.website_form} onSubmit={handleSubmit((data) => onSubmit(data))}>
         <input id="website-input" className={styles.website_input} {...register("website")} type="text" placeholder="Website URL" />
-        <span className={styles.website_error}>{errors.website?.message}</span><br />
+        {errorDisplay}
       </form>
     </section>
   )
