7734ebeb9937faa2216796de43cb04a6b5ac4e5d
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-06-29 10:33:20 -0400

    make display scroll to bottom when new question/answer is added

--- comps/Display.js
+++ comps/Display.js
@@ -1,13 +1,25 @@
 import Card from './Card';
 import styles from './Display.module.css';
+import { useEffect, useRef } from 'react'
 
 const Display = ({log, loading}) => {
+
+  const bottomRef = useRef(null);
+
   const cardsList = log.map((value, index) => {
     return <Card key={index} question={value.question} answer={value.answer} loading={loading}/>
   })
+
+  useEffect(() => {
+
+    bottomRef.current.scrollIntoView({ behavior: 'smooth' })
+
+  }, [log])
+
   return (
     <section className={styles.display}>
       {cardsList}
+      <div ref={bottomRef}></div>
     </section>
   )
 }
