155447efea4bc5b77a0688e8e465d69805a6d18b
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-07-03 14:57:58 -0400

    adjust text for footer

--- comps/Footer.js
+++ comps/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
     <footer>
 
       <div className='footer_title'>g r a m m e r h u b</div>
-      <div className='footer_info'>KEEP IN TOUCH</div>
+      {/* <div className='footer_info'>KEEP IN TOUCH</div>
 
       <div className='follow_us'>Follow us at:
         <box-icon type='logo' name='facebook'></box-icon>
         <box-icon type='logo' name='discord-alt'></box-icon>
         <box-icon name='linkedin' type='logo' ></box-icon>
-      </div>
+      </div> */}
 
       <div className='acknowledgements'>
         <p>Built by: Zephyr, Steve, Andrea & Jake</p>
