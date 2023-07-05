e7a564db1a86f8251a7ff4dba9bd852a63a46192
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-07-05 14:59:03 -0400

    display error message when question input is invalid

--- comps/QuestionForm.js
+++ comps/QuestionForm.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './QuestionForm.module.css'
+import { ErrorMessage } from '@hookform/error-message'
 
 const QuestionForm = ({ handleSubmit, onSubmit, register, errors }) => {
+
   return (
     <section className={styles.form_container}>
       <form className={styles.question_form} onSubmit={handleSubmit((data) => onSubmit(data))}>
-        <input id="question-input" className={styles.question_input} {...register("question")} type="text" placeholder="Question" />
-        {/* <span>{errors.question?.message}</span><br /> */}
+        <input id="question-input" type="text" className={styles.question_input} {...register("question")} placeholder="Question"/>
+        <ErrorMessage
+          errors={errors}
+          name="question"
+          render={({ message }) => <p class={styles.question_error}>{message}</p>}
+        />
         <input type="button" value="S u b m i t" className={styles.form_button} onClick={handleSubmit((data) => onSubmit(data))} />
       </form>
     </section>
